11c30f9f5868b1df4d0e5b94536d8052002a60f8
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-06-30 19:58:42 +0700

    done

---
+++ ImageProcessing.py
@@ -0,0 +1,112 @@
+import os
+
+import cv2 as cv
+import numpy as np
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QImage, QPixmap
+from PySide2.QtWidgets import QFileDialog, QVBoxLayout, QWidget
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+
+class MplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=5, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.subplots()
+        super(MplCanvas, self).__init__(self.fig)
+
+
+class ImageProcessing:
+
+    def __init__(self, mainWindow):
+        self.sc = MplCanvas(self, width=5, height=5, dpi=100)
+        self.toolbar = NavigationToolbar(self.sc, mainWindow)
+        self.mainWindow = mainWindow
+        self.canvaslayout = QVBoxLayout()
+        self.canvaslayout.addWidget(self.toolbar)
+        self.canvaslayout.addWidget(self.sc)
+        self.canvasWidget = QWidget()
+        self.canvasWidget.setFixedSize(500, 500)
+        self.canvasWidget.setLayout(self.canvaslayout)
+        self.mainWindow.page_2.layout().addWidget(self.canvasWidget)
+
+    def setImg(self, central_widget):
+        path_to_file, _ = QFileDialog.getOpenFileName(central_widget, "Select Image", os.path.dirname(__file__),
+                                                      "Images (*.jpg *.bmp *.png)")
+        if path_to_file != '':
+            self.imgRGB = cv.imread(path_to_file)
+            self.outputImage = self.imgRGB
+            self.imgRGB = cv.cvtColor(self.imgRGB, cv.COLOR_BGR2RGB)
+            self.displayImage(self.imgRGB, QImage.Format_RGB888, central_widget)
+            central_widget.actionExport_FIle.setEnabled(1)
+
+    def exportImage(self, central_widget):
+        path_to_file, _ = QFileDialog.getSaveFileName(central_widget, "Select location",os.path.dirname(__file__),"Images (*.jpg *.bmp *.png)")
+        if path_to_file != '':
+            cv.imwrite(path_to_file, self.outputImage)
+
+    def setGrayImage(self, central_widget):
+        central_widget.stackedWidget.setCurrentIndex(0)
+        imgR = np.array(self.imgRGB[:, :, 0])
+        imgG = np.array(self.imgRGB[:, :, 1])
+        imgB = np.array(self.imgRGB[:, :, 2])
+        self.imgGray = np.uint8(np.add(np.add(0.299 * np.float16(imgR), 0.587 * np.float16(imgG)), 0.114 * np.float16(imgB)))
+        self.outputImage = self.imgGray
+        self.displayImage(self.imgGray, QImage.Format_Grayscale8, central_widget)
+
+    def displayImage(self, img, fromatImage, central_widget):
+        central_widget.stackedWidget.setCurrentIndex(0)
+        img = QImage(img, img.shape[1], img.shape[0], img.strides[0], fromatImage)
+        central_widget.label.setPixmap(QPixmap.fromImage(img.scaled(888, 500, Qt.KeepAspectRatio)))
+
+    def histogramImage(self, centralWidget):
+        centralWidget.stackedWidget.setCurrentIndex(1)
+        imgR = np.array(self.imgRGB[:, :, 2])
+        imgG = np.array(self.imgRGB[:, :, 1])
+        imgB = np.array(self.imgRGB[:, :, 0])
+        imgGray = np.uint8(np.add(np.add(0.299 * np.float16(imgR), 0.587 * np.float16(imgG)), 0.114 * np.float16(imgB)))
+        hist, bin_edges = np.histogram(imgGray, bins=np.arange(257), density=True)
+        self.sc.axes.cla()
+        self.sc.axes.bar(np.arange(256), hist)
+        self.sc.axes.figure.canvas.draw()
+        img = QImage(imgGray, imgGray.shape[1], imgGray.shape[0], imgGray.strides[0], QImage.Format_Grayscale8)
+
+        self.outputImage = imgGray
+
+        centralWidget.label_2.setPixmap(QPixmap.fromImage(img.scaled(888, 500, Qt.KeepAspectRatio)))
+
+    def histogramEqualization(self, centralWidget):
+        centralWidget.stackedWidget.setCurrentIndex(1)
+        imgR = np.array(self.imgRGB[:, :, 2])
+        imgG = np.array(self.imgRGB[:, :, 1])
+        imgB = np.array(self.imgRGB[:, :, 0])
+        imgGray = np.uint8(np.add(np.add(0.299 * np.float16(imgR), 0.587 * np.float16(imgG)), 0.114 * np.float16(imgB)))
+        hist, bin_edges = np.histogram(imgGray, bins=np.arange(257), density=True)
+
+        cumsum = np.cumsum(hist) * 255
+        outImg = np.zeros(imgGray.shape)
+
+
+        for i in range(0, imgGray.shape[0]):
+            for j in range(0, imgGray.shape[1]):
+                outImg[i][j] = cumsum[imgGray[i][j]]
+
+        outImg = np.uint8(np.round(outImg))
+
+        outhist, newbin_edges = np.histogram(outImg, bins=np.arange(257), density=True)
+
+        self.sc.axes.cla()
+        self.sc.axes.bar(np.arange(256), outhist)
+        self.sc.axes.figure.canvas.draw()
+
+        self.outputImage = outImg
+
+        img = QImage(np.uint8(outImg), np.uint8(outImg).shape[1], np.uint8(outImg).shape[0], np.uint8(outImg).strides[0], QImage.Format_Grayscale8)
+        centralWidget.label_2.setPixmap(QPixmap.fromImage(img.scaled(888, 500, Qt.KeepAspectRatio)))
+
+
+
---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+
+import cv2 as cv
+from PySide2.QtGui import QImage
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile, QIODevice, Slot
+
+from ImageProcessing import ImageProcessing
+
+
+class Mainwindow:
+    def __init__(self):
+        ui_file_name = "mainwindow.ui"
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+            sys.exit(-1)
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+        if not self.window :
+            print(loader.errorString())
+            sys.exit(-1)
+        self.window.show()
+        self.myImageProcessing = ImageProcessing(self.window)
+
+        self.window.stackedWidget.setCurrentIndex(0)
+
+        self.window.actionExport_FIle.setEnabled(0)
+
+        self.window.actionImport_File.triggered.connect(self.importImage)
+        self.window.actionExport_FIle.triggered.connect(self.exportImage)
+        self.window.actionGray_Image.triggered.connect(self.grayScale)
+        self.window.actionRGB_Image.triggered.connect(self.rgbScale)
+        self.window.Histogram_EqualizationNo.triggered.connect(self.histogramEqualizationOff)
+        self.window.Histogram_EqualizationYes.triggered.connect(self.histogramEqualizationOn)
+
+    @Slot()
+    def importImage(self):
+        self.myImageProcessing.setImg(self.window)
+
+
+    def exportImage(self):
+        self.myImageProcessing.exportImage(self.window)
+
+    def rgbScale(self):
+        self.myImageProcessing.displayImage(self.myImageProcessing.imgRGB, QImage.Format_RGB888, self.window)
+        self.myImageProcessing.outputImage = cv.cvtColor(self.myImageProcessing.imgRGB, cv.COLOR_BGR2RGB)
+
+    def grayScale(self):
+        self.myImageProcessing.setGrayImage(self.window)
+
+    def histogramEqualizationOff(self):
+        self.myImageProcessing.histogramImage(self.window)
+
+    def histogramEqualizationOn(self):
+        self.myImageProcessing.histogramEqualization(self.window)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    my_main_window = Mainwindow()
+    sys.exit(app.exec_())
